7f88394379378995d530bdaf978e4dae0d68aeef
Chun-Min Chang <chun.m.chang@gmail.com>
2016-03-16 17:16:00 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dotfiles
 Common environment settings
+
+# Notes
+After setup, the following local file will be moved to your __Trash__ can
+- $HOME/.gitconfig
+- $HOME/.hgrc
+- $HOME/.bash_profile, if OS X
+- $HOME/.bashrc, if Linux-based OS
--- setup.sh
+++ setup.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 source utils.sh
 
-GrepStringInFile jekku ~/.gitconfi
-
 # Link environment setting
 # -------------------------------------------------------------
+trash=$(GetTrashPath)
+
 LogH1 "Link environment setting"
 # If the environment is mac, then link the bash_profile to $HOME/.bash_profile
-# if [ $ENV_OSX == $(GetOSEnvironment) ]; then
-#   ln -s $(pwd)/bash_profile $HOME/.bash_profile
-# elif [ $ENV_LINUX == $(GetOSEnvironment) ]; then
-#   ln -s $(pwd)/bashrc $HOME/.bashrc
-# fi
+if [ $ENV_OSX == $(GetOSEnvironment) ]; then
+  bashProfile="$HOME/.bash_profile"
+
+  exist=$(DoseFileExist $bashProfile)
+
+  if [ $exist -eq 1 ]; then # if file exist, then remove it to Trash first
+    mv $bashProfile $trash
+  fi
+
+  # Link the bash_profile
+  echo "link .bash_profile to bash_profile here"
+  ln -s $(pwd)/bash_profile $bashProfile
+
+elif [ $ENV_LINUX == $(GetOSEnvironment) ]; then
+  bashRc="$HOME/.bashrc"
+
+  exist=$(DoseFileExist $bashRc)
+
+  if [ $exist -eq 1 ]; then # if file exist, then remove it to Trash first
+    mv $bashRc $trash
+  fi
+
+  echo  "link .bashrc to bashrc here"
+  # ln -s $(pwd)/bashrc $bashRc
+fi
 
 # Git setting
 # -------------------------------------------------------------
