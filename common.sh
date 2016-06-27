fa53e1719e5ab5d7bc0b4f03a1640075ad3fc08b
chunminchang <cchang@mozilla.com>
2016-06-27 10:43:53 +0800

    git mozreview

--- common.sh
+++ common.sh
@@ -22,10 +22,12 @@ linkMachrc
 # Mozillla Git Reviewboard:
 # --------------------------------------------------------------------
 # For git-cinnabar
-ExportToPATH /Users/chunminchang/Work/git-cinnabar
+ExportToPATH /Users/chunminchang/Work/git-cinnabar # my MacPro@Mozilla
+ExportToPATH /home/chunmin/Work/git-cinnabar # my Ubuntu@Mozilla
 
 # For version-control-tools
-ExportToPATH /Users/chunminchang/.mozbuild/version-control-tools/git/commands
+ExportToPATH /Users/chunminchang/.mozbuild/version-control-tools/git/commands # my MacPro@Mozilla
+ExportToPATH /home/chunmin/.mozbuild/version-control-tools/git/commands # my Ubuntu@Mozilla
 
 # Git alias
 # --------------------------------------------------------------------
