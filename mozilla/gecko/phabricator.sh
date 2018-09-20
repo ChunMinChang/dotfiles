7ebddb4030d09963be83076450c0aca4bf276956
Chun-Min Chang <chun.m.chang@gmail.com>
2018-09-20 14:10:24 -0700

    load arcanist and git-cinnabar from default paths

--- mozilla/gecko/phabricator.sh
+++ mozilla/gecko/phabricator.sh
@@ -1,1 +1,13 @@
-export PATH=/Users/cchang/Work/arcanist/bin/:$PATH
+ARCANIST_BIN=$HOME/Work/arcanist/bin/
+if [ -d $ARCANIST_BIN ]; then
+  export PATH=$ARCANIST_BIN:$PATH
+else
+  echo "Please update path for arcanist commands"
+fi
+
+GIT_CINNABAR=$HOME/Work/git-cinnabar/
+if [ -d $GIT_CINNABAR ]; then
+  export PATH=$GIT_CINNABAR:$PATH
+else
+  echo "Please update path for git-cinnabar commands"
+fi
