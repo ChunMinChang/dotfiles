4fd699ac81a91eb65700f52db96ccd958043d939
Chun-Min Chang <chun.m.chang@gmail.com>
2018-09-21 15:15:07 -0700

    Remove alias for mozreview

--- mozilla/gecko/alias.sh
+++ mozilla/gecko/alias.sh
@@ -11,8 +11,3 @@ alias minst='./mach install'
 alias mm='./mach mochitest'
 # alias mmt='./mach mochitest [your_parameters] [your_test_file]'
 
-# mozreview
-# 1. git version mozreview
-alias gmr='git mozreview push'
-# 2. hg version mozreview
-alias hmr='hg push review'
