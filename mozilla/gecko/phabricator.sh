2164ab4ca0ccb3b0ab9e4cdd91e23814d66b72e1
Chun-Min Chang <chun.m.chang@gmail.com>
2019-11-19 14:32:37 -0800

    Remove moz-phab in $PATH
    Since moz-phab is published via pip, there is no need to keep its source
    code in file and export its path.

--- mozilla/gecko/phabricator.sh
+++ mozilla/gecko/phabricator.sh
@@ -11,10 +11,3 @@ if [ -d $GIT_CINNABAR ]; then
 else
   PrintError 'No git-cinnabar command!\nRun: "$ ./mach bootstrap" under gecko repo to fix it!'
 fi
-
-MOZ_PHAB=$HOME/Work/review/
-if [ -d $MOZ_PHAB ]; then
-  export PATH=$MOZ_PHAB:$PATH
-else
-  PrintError 'Please update path for moz-phab commands'
-fi
