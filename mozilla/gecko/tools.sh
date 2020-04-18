0dcbda95d151b563ec2615fe071fab9ac376a314
Chun-Min Chang <chun.m.chang@gmail.com>
2020-04-18 10:14:43 -0700

    Don't check moz-phab under .mozbuild

--- mozilla/gecko/tools.sh
+++ mozilla/gecko/tools.sh
@@ -8,7 +8,6 @@ else
   PrintError 'No git-cinnabar command!\nRun: "$ ./mach bootstrap" under gecko repo to fix it!'
 fi
 
-MOZPHAB=$HOME/.mozbuild/moz-phab
-if [ $(CommandExists moz-phab) -eq 0 ] || [ ! -d $MOZPHAB ]; then
+if [ $(CommandExists moz-phab) -eq 0 ]; then
   PrintError 'No moz-phab command!\nInstall moz-phab: https://moz-conduit.readthedocs.io/en/latest/phabricator-user.html!'
 fi
