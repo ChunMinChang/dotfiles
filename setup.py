0b2e7e2b347bf15f78710a7deac18b2242d5000d
Chun-Min Chang <chun.m.chang@gmail.com>
2022-04-01 10:04:20 -0700

    format

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ import sys
 
 # Global variables
 # ------------------------------------------------------------------------------
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 HOME_DIR = os.environ['HOME']
 
@@ -24,6 +25,7 @@ class colors:
 
 # Utils
 # ------------------------------------------------------------------------------
+
 # Symbolically link source to target
 def link(source, target):
     if os.path.islink(target):
@@ -43,19 +45,23 @@ def is_tool(name):
     except:
         return False
 
-def append_to_next_line_after(name, pattern, value = ''):
-    file = fileinput.input(name, inplace = True)
+
+def append_to_next_line_after(name, pattern, value=''):
+    file = fileinput.input(name, inplace=True)
     for line in file:
         replacement = line + ('\n' if '\n' not in line else '') + value
         line = re.sub(pattern, replacement, line)
         sys.stdout.write(line)
     file.close()
 
+
 def bash_export_command(path):
-    return ''.join(['export PATH=', path,':$PATH'])
+    return ''.join(['export PATH=', path, ':$PATH'])
+
 
 def bash_load_command(path):
     return ''.join(['[ -r ', path, ' ] && . ', path])
+
 
 def append_nonexistent_lines_to_file(file, lines):
     with open(file, 'r+') as f:
@@ -74,23 +80,27 @@ def append_nonexistent_lines_to_file(file, lines):
         print(content)
         f.close()
 
+
 def print_installing_title(name, bold=False):
     print(colors.HEADER + ''.join(['\n', name,
-        ('\n==============================' if bold
-         else '\n--------------------')]) + colors.END)
+                                   ('\n==============================' if bold
+                                    else '\n--------------------')]) + colors.END)
 
 # TODO: Use Print{Error, Hint, Warning} instead
 def print_hint(message):
     print(colors.HINT + message + colors.END + '\n')
 
+
 def print_warning(message):
     print(colors.WARNING + 'WARNING: ' + message + colors.END + '\n')
+
 
 def print_fail(message):
     print(colors.FAIL + 'ERROR: ' + message + colors.END + '\n')
 
 # Setup functions
 # ------------------------------------------------------------------------------
+
 # Link this dotfiles path to $HOME/.dotfiles
 def dotfiles_link():
     print_installing_title('dotfile path')
@@ -118,18 +128,19 @@ def bash_link():
             'dot.zshrc' if v >= 10.15 else 'dot.bash_profile')
 
     #files = filter(lambda f: f.startswith('dot.'), os.listdir(BASE_DIR))
-    files = platform_files[platform.system()];
+    files = platform_files[platform.system()]
     for f in files:
-        target = os.path.join(HOME_DIR, f[3:]) # Get name after dot
+        target = os.path.join(HOME_DIR, f[3:])  # Get name after dot
         src = os.path.join(BASE_DIR, f)
         if os.path.isfile(target):
             if os.path.samefile(src, target):
-                print_warning('{} is already linked!'.format(target));
+                print_warning('{} is already linked!'.format(target))
                 continue
-            print_warning('{} already exists!'.format(target));
+            print_warning('{} already exists!'.format(target))
             if f == 'dot.bashrc':
                 print('Append a command to load {} in {}'.format(src, target))
-                append_nonexistent_lines_to_file(target, [bash_load_command(src)])
+                append_nonexistent_lines_to_file(
+                    target, [bash_load_command(src)])
             else:
                 print_warning('Do nothing.')
         else:
@@ -144,10 +155,13 @@ def git_init():
 
     git_config = HOME_DIR + '/.gitconfig'
     if not os.path.isfile(git_config):
-        print_warning('{} does not exist! Create a new one with default settings!'.format(git_config))
+        print_warning(
+            '{} does not exist! Create a new one with default settings!'.format(git_config))
         # Set global user name and email
-        subprocess.call(['git', 'config', '--global', 'user.name', 'Chun-Min Chang'])
-        subprocess.call(['git', 'config', '--global', 'user.email', 'chun.m.chang@gmail.com'])
+        subprocess.call(['git', 'config', '--global',
+                        'user.name', 'Chun-Min Chang'])
+        subprocess.call(['git', 'config', '--global',
+                        'user.email', 'chun.m.chang@gmail.com'])
 
     # Include git config here in global gitconfig file
     path = BASE_DIR + '/git/config'
@@ -162,11 +176,12 @@ def git_init():
 
 # mozilla stuff
 # ---------------------------------------
+
 def mozilla_init():
     print_installing_title('mozilla settings', True)
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mozilla', nargs = '*',
-                        help = 'Installing the toolkit for developing gecko')
+    parser.add_argument('--mozilla', nargs='*',
+                        help='Installing the toolkit for developing gecko')
     args = parser.parse_args()
 
     if args.mozilla is None:
@@ -174,16 +189,17 @@ def mozilla_init():
         return
 
     funcs = {
-      'gecko': gecko_init,
-      'hg': hg_init,
-      'tools': tools_init,
-      'rust': rust_init,
+        'gecko': gecko_init,
+        'hg': hg_init,
+        'tools': tools_init,
+        'rust': rust_init,
     }
 
     options = (set(funcs.keys()).intersection(set(args.mozilla)) if args.mozilla
                else funcs.keys())
     for k in options:
         funcs[k]()
+
 
 def gecko_init():
     print_installing_title('gecko alias and machrc')
@@ -203,23 +219,25 @@ def gecko_init():
     path = BASE_DIR + '/mozilla/gecko/alias.sh'
     append_nonexistent_lines_to_file(bashrc, [bash_load_command(path)])
 
+
 def hg_init():
     print_installing_title('hg settings')
     error_messages = ['\tRun ./mach bootstrap.py under gecko-dev to fix it.']
 
     if not is_tool('hg'):
-        error_messages.insert(0, 'Please install hg(mercurial) first!');
+        error_messages.insert(0, 'Please install hg(mercurial) first!')
         print_fail(''.join(error_messages))
         return
 
     hg_config = HOME_DIR + '/.hgrc'
     if not os.path.isfile(hg_config):
-        error_messages.insert(0, '{} does not exist! Abort!'.format(hg_config));
+        error_messages.insert(0, '{} does not exist! Abort!'.format(hg_config))
         print_fail(''.join(error_messages))
         return
 
     path = BASE_DIR + '/mozilla/hg/config'
     append_nonexistent_lines_to_file(hg_config, ['%include ' + path])
+
 
 def tools_init():
     print_installing_title('tools settings')
@@ -232,6 +250,7 @@ def tools_init():
     path = BASE_DIR + '/mozilla/gecko/tools.sh'
     append_nonexistent_lines_to_file(bashrc, [bash_load_command(path)])
 
+
 def rust_init():
     print_installing_title('rust settings')
     error_messages = ['\tRun ./mach bootstrap.py under gecko-dev to fix it.']
@@ -243,11 +262,12 @@ def rust_init():
 
     cargo_env = HOME_DIR + '/.cargo/env'
     if not os.path.isfile(cargo_env):
-        error_messages.insert(0, '{} does not exist! Abort!'.format(cargo_env));
+        error_messages.insert(0, '{} does not exist! Abort!'.format(cargo_env))
         print_fail(''.join(error_messages))
         return
 
     append_nonexistent_lines_to_file(bashrc, [bash_load_command(cargo_env)])
+
 
 def main(argv):
     dotfiles_link()
@@ -258,6 +278,7 @@ def main(argv):
     mozilla_init()
 
     print_hint('Please run `$ source ~/.bachrc` turn on the environment settings')
+
 
 if __name__ == '__main__':
     try:
